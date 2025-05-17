9fa27fddf3f95dfba0b25589ee91a5fe668446b9
Nika Alexandrovna <megrelynika@gmail.com>
2025-05-17 20:41:23 +0400

    add functions for tests

---
+++ src/processing.py
@@ -0,0 +1,49 @@
+from pprint import pprint
+
+
+def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    Args:
+        list_of_dicts: Список словарей, каждый из которых предположительно содержит ключ 'state'.
+        state: Опциональное значение для ключа 'state', по которому будет производиться фильтрация.
+               По умолчанию установлено значение 'EXECUTED'.
+
+    Returns:
+        Новый список словарей, содержащий только те словари, у которых значение
+        ключа 'state' соответствует указанному значению.
+    """
+
+    list_of_dictionary = []
+    for item in list_of_dicts:
+        if "state" in item and item["state"] == state:
+            list_of_dictionary.append(item)
+    return list_of_dictionary
+
+
+def sort_by_date(list_of_dicts: list[dict], desc: bool = True) -> list[dict]:
+    """Сортирует список словарей по значению ключа 'date'.
+
+    Args:
+        list_of_dicts: Список словарей, каждый из которых должен содержать ключ 'date'
+                       со строковым представлением даты.
+        desc: Необязательный булевый параметр. Если True (по умолчанию), сортировка
+              выполняется по убыванию (от новых дат к старым). Если False,
+              сортировка выполняется по возрастанию (от старых дат к новым).
+
+    Returns:
+        Новый список словарей, отсортированный по значению ключа 'date'.
+    """
+    return sorted(list_of_dicts, key=lambda item: item["date"], reverse=desc)
+
+
+input_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+pprint(filter_by_state(input_data))
+pprint(sort_by_date(input_data))
