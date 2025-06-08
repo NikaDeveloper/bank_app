8889f7bfe9ebd1855d9b8b918ecedb484726ef1b
Nika Alexandrovna <megrelynika@gmail.com>
2025-06-08 20:55:16 +0400

    create data/

--- src/utils.py
+++
@@ -1,3 +0,0 @@
-def text_reverse(new_string: str) -> str:
-    """Переворачивает строку"""
-    return new_string[::-1]
--- tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from src.utils import text_reverse
-
-
-def test_text_reverse():
-    assert text_reverse("bank") == "knab"
-    assert text_reverse("") == ""
-    assert text_reverse("a") == "a"
