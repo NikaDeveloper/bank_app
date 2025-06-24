2c418810e0635ffcc1fad3d426042a835ce37cf2
Nika Alexandrovna <megrelynika@gmail.com>
2025-06-24 22:51:46 +0400

    merge develop into main

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,11 +1,7 @@
 import pytest
 
-from src.processing import (
-    filter_by_state,
-    process_bank_operations,
-    process_bank_search,
-    sort_by_date,
-)
+from src.processing import (filter_by_state, process_bank_operations,
+                            process_bank_search, sort_by_date)
 
 
 # Тесты для filter_by_state
